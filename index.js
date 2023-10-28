dcb326ae9a1e285b50e15460c4cc9b0d53176187
Epiktet17 <tim.letnar@gmail.com>
2023-10-28 07:43:50 +0200

    Functioning UI and logic

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
-/*SLIDER JS COPIED FROM W3*/
+let gridContainer = document.querySelector(".grid-container");
+
+
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value;
+  clearGrid();
+  createGrid(this.value);
+  setColor();
 }
-/*SLIDER JS COPIED FROM W3*/
+
+function createGrid(sizeOfGrid){//only need 1 variable, if grid has 3 rows it has to have 3 columns
+  for (let i = 0; i < sizeOfGrid; i++) {
+    let row = document.createElement("div");
+
+    row.classList.add("grid-row");
+    gridContainer.appendChild(row);
+    for (let j = 0; j < sizeOfGrid; j++) {
+      let divInsideRow = document.createElement("div");
+      divInsideRow.classList.add('div-inside-row');
+      row.appendChild(divInsideRow);
+    }
+  }
+}
+
+function clearGrid() {
+  let rows = document.querySelectorAll(".grid-row");
+  rows.forEach(function(row) {
+    row.remove();
+  });
+}
+
+function colorGrid(color){
+  let divInsideSelector = document.querySelectorAll(".div-inside-row");
+  for (let i = 0; i < divInsideSelector.length; i++) {
+    divInsideSelector[i].addEventListener("mouseover",function(){
+      divInsideSelector[i].style.backgroundColor = color;
+    });
+  }
+  }
+function eraser(){
+  document.getElementById("favcolor").value = "#FFFFFF";
+}
+
+function setColor(){
+  let pickedColor = document.getElementById("favcolor").value;
+  colorGrid(pickedColor);
+}
